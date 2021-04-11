eb075a2293c32853abe4fcb2135fd5f3ce73dfe6
John Michael Bacasno <79495944+johnmichaelbacasno@users.noreply.github.com>
2021-04-11 20:45:11 +0800

    Update view.py

--- Simple Student Management System/view.py
+++ Simple Student Management System/view.py
@@ -10,7 +10,7 @@ class View:
         self.root.title('Student Profile')
         self.root.geometry('450x575')
         self.root.resizable(width=False, height=False)
-        self.root.configure(background ='#FFFFFF')
+        self.root.configure(background='#FFFFFF')
         
         self.key = key
         self.fields = ('id', 'name', 'gender', 'course', 'year', 'birthdate', 'contact', 'address')
